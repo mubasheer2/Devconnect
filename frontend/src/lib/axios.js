51252b7b0d75bfb027b57ded75783d8ab2dbbbe8
mubasheer2 <shaikhmubbi5786@gmail.com>
2026-05-16 19:50:40 +0530

    fix production auth

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:5001/api"
-    : import.meta.env.VITE_API_URL;
+    : "https://devconnect-56tm.onrender.com/api";  // /api add kar
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
