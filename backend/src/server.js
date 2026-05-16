3a140ac6975f7204cc56023de96523c578f96b0b
mubasheer2 <shaikhmubbi5786@gmail.com>
2026-05-16 18:17:33 +0530

    render deployment fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import path from "path";
+
 import uploadRoute from "./routes/upload.route.js";
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
@@ -11,17 +12,18 @@ import chatRoutes from "./routes/chat.route.js";
 import { connectDB } from "./lib/db.js";
 
 const app = express();
-const PORT = process.env.PORT;
+
+const PORT = process.env.PORT || 5001;
+
 const __dirname = path.resolve();
 
 app.use(
   cors({
-    origin: "http://localhost:3001",
+    origin: true,
     credentials: true,
   })
 );
 
-// 🔥 IMPORTANT FIX
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
@@ -31,11 +33,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/upload", uploadRoute);
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
 
