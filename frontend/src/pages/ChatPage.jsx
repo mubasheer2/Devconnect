7b3755ec41ba1e8e34c60881df702da1e3a07f73
mubasheer2 <shaikhmubbi5786@gmail.com>
2026-05-21 00:36:33 +0530

    fixed external tool links

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,4 +1,4 @@
-mport { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import useAuthUser from "../hooks/useAuthUser";
 import { useQuery } from "@tanstack/react-query";
